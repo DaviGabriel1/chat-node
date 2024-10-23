ed525ab781cb3d36684329daa885c054bab45f60
DaviGabriel1 <davig41441503@gmail.com>
2024-10-23 17:23:52 -0300

    Dashboard criado

--- routes/toughtsRoutes.js
+++ routes/toughtsRoutes.js
@@ -5,7 +5,7 @@ const ToughtsController = require("../controllers/ToughtsController")
 //helpers
 const checkAuth = require("../helpers/auth").checkAuth //protege rotas
 
-
+router.get("/add",checkAuth ,ToughtsController.createTought)
 router.get("/dashboard",checkAuth ,ToughtsController.dashboard)
 router.get("/", ToughtsController.showToughts)
 
