dab53272eb54d79496a93007ee42e8a8baa0e95f
DaviGabriel1 <davig41441503@gmail.com>
2024-10-24 10:14:02 -0300

    processo de edição de publicações

--- controllers/ToughtsController.js
+++ controllers/ToughtsController.js
@@ -18,7 +18,14 @@ module.exports = class ToughtsController {
             res.redirect("/login")
         }
         const toughts = user.Toughts.map((result) => result.dataValues)
-        res.render("toughts/dashboard", {toughts })
+
+        let emptyToughts = false;
+
+        if(toughts.length === 0){
+            emptyToughts = true
+        }
+
+        res.render("toughts/dashboard", {toughts,emptyToughts })
     }
 
     static createTought(req,res){
@@ -57,7 +64,31 @@ module.exports = class ToughtsController {
         }catch(err){
             console.log(err)
         }
+    }
 
+    static async editTought(req,res){
+        const id = req.params.id;
 
+        const tought = await Tought.findOne({where:{id:id},raw:true})
+
+        res.render("toughts/edit",{tought})
+    }
+
+    static async editToughtSave(req,res){
+        const id = req.body.id
+        const tought = {
+            title:req.body.title
+        }
+        
+        try{
+            await Tought.update(tought,{where:{id:id}})
+        req.flash("message","pensamento atualizado com sucesso!")
+
+        req.session.save(() => {
+            res.redirect("/toughts/dashboard")
+        })
+        }catch(err){
+            console.log(err)
+        }
     }
 }
--- routes/toughtsRoutes.js
+++ routes/toughtsRoutes.js
@@ -7,6 +7,8 @@ const checkAuth = require("../helpers/auth").checkAuth //protege rotas
 
 router.get("/add",checkAuth ,ToughtsController.createTought)
 router.post("/add",checkAuth ,ToughtsController.createToughtSave)
+router.get("/edit/:id",checkAuth ,ToughtsController.editTought)
+router.post("/edit",checkAuth ,ToughtsController.editToughtSave)
 router.get("/dashboard",checkAuth ,ToughtsController.dashboard)
 router.post("/remove",checkAuth ,ToughtsController.removeTought)
 router.get("/", ToughtsController.showToughts)
